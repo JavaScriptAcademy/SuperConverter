97c5cce5f4a6a5f099277113aa3210b5bf658025
Claire Liu <Claire_Liu@epam.com>
2016-04-15 14:09:31 +0800

    Finish the temperature page

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,18 +1,18 @@
 angular.module('app.controllers', [])
-  
+
 .controller('currencyCtrl', function($scope) {
 
 })
-   
+
 .controller('lengthCtrl', function($scope) {
 
 })
-   
+
 .controller('weightCtrl', function($scope) {
 
 })
-      
+
 .controller('temperatureCtrl', function($scope) {
+  $scope.temperatureOptions = ['CELSIUS', 'FAHENHEIT', 'KELVIN', 'RANKINE', 'REAUMUR'];
 
 })
- 
